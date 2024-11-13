e1dedcfef792536d7f142d07450bb5562d629aa7
Matěj Šubrt <matej.subrt@gmail.com>
2024-11-13 13:18:54 +0100

    Change SQLite package from System.Data.DQLite to Microsoft.Data.Sqlite to allow linux compatibility

--- RAPTOR-Router/RAPTOR-Router/GBFSParsing/Distances/BikeDistanceDatabase.cs
+++ RAPTOR-Router/RAPTOR-Router/GBFSParsing/Distances/BikeDistanceDatabase.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Data.SQLite;
+using Microsoft.Data.Sqlite;
 using RAPTOR_Router.GBFSParsing.Distances;
 using RAPTOR_Router.Structures.Bike;
 
@@ -15,7 +15,7 @@ public class BikeDistanceDatabase
 
     private void CreateDatabaseAndTable()
     {
-        using (var connection = new SQLiteConnection(dbPath))
+        using (var connection = new SqliteConnection(dbPath))
         {
             connection.Open();
 
@@ -27,7 +27,7 @@ public class BikeDistanceDatabase
                     PRIMARY KEY (StationA, StationB)
                 )";
 
-            using (var command = new SQLiteCommand(createTableQuery, connection))
+            using (var command = new SqliteCommand(createTableQuery, connection))
             {
                 command.ExecuteNonQuery();
             }
@@ -38,7 +38,7 @@ public class BikeDistanceDatabase
 
     public void AddOrUpdateDistance(string stationA, string stationB, double distance)
     {
-        using (var connection = new SQLiteConnection(dbPath))
+        using (var connection = new SqliteConnection(dbPath))
         {
             connection.Open();
 
@@ -47,7 +47,7 @@ public class BikeDistanceDatabase
             INSERT OR REPLACE INTO Distances (StationA, StationB, Distance) 
             VALUES (@StationA, @StationB, @Distance)";
 
-            using (var command = new SQLiteCommand(insertQuery, connection))
+            using (var command = new SqliteCommand(insertQuery, connection))
             {
                 command.Parameters.AddWithValue("@StationA", stationA);
                 command.Parameters.AddWithValue("@StationB", stationB);
@@ -61,7 +61,7 @@ public class BikeDistanceDatabase
 
     public double GetDistance(string stationA, string stationB)
     {
-        using (var connection = new SQLiteConnection(dbPath))
+        using (var connection = new SqliteConnection(dbPath))
         {
             connection.Open();
 
@@ -71,7 +71,7 @@ public class BikeDistanceDatabase
                 WHERE (StationA = @StationA AND StationB = @StationB) 
                    OR (StationA = @StationB AND StationB = @StationA)";
 
-            using (var command = new SQLiteCommand(selectQuery, connection))
+            using (var command = new SqliteCommand(selectQuery, connection))
             {
                 command.Parameters.AddWithValue("@StationA", stationA);
                 command.Parameters.AddWithValue("@StationB", stationB);
@@ -82,13 +82,13 @@ public class BikeDistanceDatabase
         }
     }
 
-    private void RemoveStation(SQLiteConnection connection, string station)
+    private void RemoveStation(SqliteConnection connection, string station)
     {
         string deleteQuery = @"
             DELETE FROM Distances 
             WHERE StationA = @Station OR StationB = @Station";
 
-        using (var command = new SQLiteCommand(deleteQuery, connection))
+        using (var command = new SqliteCommand(deleteQuery, connection))
         {
             command.Parameters.AddWithValue("@Station", station);
             command.ExecuteNonQuery();
@@ -97,7 +97,7 @@ public class BikeDistanceDatabase
 
     public StationDistanceMatrix GetDistanceMatrixAndRemoveNonExistentStations(Dictionary<string, BikeStation> stationsById)
     {
-        using (var connection = new SQLiteConnection(dbPath))
+        using (var connection = new SqliteConnection(dbPath))
         {
             var matrix = new StationDistanceMatrix();
 
@@ -105,7 +105,7 @@ public class BikeDistanceDatabase
 
             string selectQuery = "SELECT StationA, StationB, Distance FROM Distances";
 
-            using (var command = new SQLiteCommand(selectQuery, connection))
+            using (var command = new SqliteCommand(selectQuery, connection))
             {
                 using (var reader = command.ExecuteReader())
                 {
