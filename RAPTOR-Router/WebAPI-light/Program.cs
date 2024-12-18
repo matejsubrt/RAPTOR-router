752ee19a8e297f7c0760e948435bbcd704e3a0f5
Matěj Šubrt <matej.subrt@gmail.com>
2024-12-18 18:14:23 +0100

    Remove API debug prints

--- RAPTOR-Router/WebAPI-light/Program.cs
+++ RAPTOR-Router/WebAPI-light/Program.cs
@@ -123,37 +123,8 @@ namespace WebAPI_light
 
         static IResult HandleUpdateDelaysRequest(List<SearchResult> results)
         {
-            var options = new JsonSerializerOptions
-            {
-                WriteIndented = true // Makes the output more readable with indentation
-            };
-
-            foreach(var result in results)
-            {
-                foreach(var alternative in result.UsedTripAlternatives)
-                {
-                    foreach(var trip in alternative.Alternatives)
-                    {
-                        Console.WriteLine("HasDelay: " + trip.hasDelayInfo + ", departureDelay: " + trip.delayWhenBoarded + ", currDelay: " + trip.currentDelay);
-                    }
-                }
-            }
-
-            Console.WriteLine("--------------------------------------------------------------------------");
             var delayUpdater = RouteFinderBuilder.CreateDelayUpdater();
             var newResults = delayUpdater.UpdateDelays(results);
-
-            foreach (var result in newResults)
-            {
-                foreach (var alternative in result.UsedTripAlternatives)
-                {
-                    foreach (var trip in alternative.Alternatives)
-                    {
-                        Console.WriteLine("HasDelay: " + trip.hasDelayInfo + ", departureDelay: " + trip.delayWhenBoarded + ", currDelay: " + trip.currentDelay);
-                    }
-                }
-            }
-
 
             return Results.Ok(newResults);
         }
